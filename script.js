76d6a804ed4ac976315faf20b007dac03e6949c7
azad.vt@techjays.com <azad.vt@techjays.com>
2024-10-19 22:29:13 +0530

    Fix NaN issue for timeSpent by initializing it to 0 in addTask function

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function renderTasks() {
 }
 
 function formatTime(seconds) {
+	seconds = seconds || 0;
 	const hrs = Math.floor(seconds / 3600);
 	const mins = Math.floor((seconds % 3600) / 60);
 	const secs = seconds % 60;
